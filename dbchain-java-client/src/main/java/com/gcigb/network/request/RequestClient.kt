d4de98c443d1346ad7bf91df7c2fd8ea8cfa9d3c
dingshaoshuai <xdingshaoshuai@163.com>
2021-10-21 09:23:22 +0800

    jdk11 -> jdk8

--- dbchain-java-client/src/main/java/com/gcigb/network/request/RequestClient.kt
+++ dbchain-java-client/src/main/java/com/gcigb/network/request/RequestClient.kt
@@ -1,6 +1,7 @@
 package com.gcigb.network.request
 
 import okhttp3.MediaType
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody
 
 /**
@@ -10,7 +11,7 @@ import okhttp3.RequestBody
 class RequestClient {
     companion object {
         fun getJsonRequestBody(json: String): RequestBody {
-            return RequestBody.create(MediaType.parse("application/json"), json)
+            return RequestBody.create("application/json".toMediaTypeOrNull(), json)
         }
     }
 }
